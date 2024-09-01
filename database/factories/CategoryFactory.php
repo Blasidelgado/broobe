5b1738a5eb7fc5abfa5be74a118cffb8a12c2b15
Blasidelgado <blasidelgado@gmail.com>
2024-09-01 05:03:47 -0300

    fix and style improvement in seeders and factories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Factories;
+use App\Models\Category;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -10,6 +11,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CategoryFactory extends Factory
 {
     /**
+    * The name of the factory's corresponding model.
+    *
+    * @var class-string<\Illuminate\Database\Eloquent\Model>
+    */
+   protected $model = Category::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -17,7 +25,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word,
+            'name' => 'ACCESSIBILITY',
         ];
     }
 }
