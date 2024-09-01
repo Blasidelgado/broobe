c8518daf5f832826dbe12c595c7c49a3e83e35d2
Blasidelgado <blasidelgado@gmail.com>
2024-09-01 20:17:27 -0300

    fix: metric_history_runs schema

--- database/migrations/2024_08_31_190234_create_metric_history_runs_table.php
+++ database/migrations/2024_08_31_190234_create_metric_history_runs_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('metric_history_runs', function (Blueprint $table) {
             $table->id();
-            $table->float('accessibility_metric', 5, 2);
-            $table->float('pwa_metric', 5, 2);
-            $table->float('performance_metric', 5, 2);
-            $table->float('seo_metric', 5, 2);
-            $table->float('best_practices_metric', 5, 2);
+            $table->string('url');
+            $table->float('accessibility_metric', 5, 2)->nullable();
+            $table->float('pwa_metric', 5, 2)->nullable();
+            $table->float('performance_metric', 5, 2)->nullable();
+            $table->float('seo_metric', 5, 2)->nullable();
+            $table->float('best_practices_metric', 5, 2)->nullable();
             $table->foreignId('strategy_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
